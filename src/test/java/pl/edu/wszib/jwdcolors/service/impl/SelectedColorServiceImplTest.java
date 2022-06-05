538638f1c950dc133f3b87a396581ef0071c70be
miki <m.miklas@wszib.edu.pl>
2022-06-05 16:17:02 +0200

    Finalizacja aplikacji

---
+++ src/test/java/pl/edu/wszib/jwdcolors/service/impl/SelectedColorServiceImplTest.java
@@ -0,0 +1,27 @@
+package pl.edu.wszib.jwdcolors.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SelectedColorServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void testSave() {
+    }
+
+    @Test
+    void getAllData() {
+    }
+}
